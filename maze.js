22969c473b0e917fa878a4b58cb142f0b8146321
Matthew DuBose <matthew.dubose@gmail.com>
2019-01-25 21:50:00 -0600

    revisions and code clean up

--- maze.js
+++ maze.js
@@ -2,168 +2,168 @@
  * Created by Matthew on 11/30/2015.
  */
 
-function clicked() {
-    var _size = document.getElementById('size').value;
-    if (_size == "") {
-        return;
-    }
-    if (parseInt(_size) > 840) {
+var _size;
+var context = document.getElementById('drawingarea').getContext("2d");
+
+var clicked = function () {
+    _size = document.getElementById('size').value;
+    if (_size === "") {
         return;
     }
     _size = parseInt(_size);
+    if (_size > 840) {
+        return;
+    }
+
     var _grid = new Array(_size * _size);
     var builderList = [];
     var cells = [];
     var random = getRandomInt(0, _size);
-    RenderWalls(_grid, _size);
-    InitializeCells(cells, _size);
-    SetupCellNeighbors(cells, _size);
+    renderWalls(_grid, _size);
+    initializeCells(cells, _size);
+    setupCellNeighbors(cells, _size);
     builderList.push(cells[random]);
     while (builderList.length > 0) {
-        builderList = BuildMaze(builderList, _grid, random);
+        builderList = buildMaze(builderList, _grid, getRandomInt(0, _size));
     }
+};
 
-    function Removewall(mazeCell, whichway, grid) {
-        if (mazeCell.GridLocation.X < whichway.GridLocation.X) {
-            grid[((mazeCell.GridLocation.X + 1) * _size) + mazeCell.GridLocation.Y] = true;
-            Draw(mazeCell.GridLocation.X + 1, mazeCell.GridLocation.Y);
-        }
-        if (mazeCell.GridLocation.X > whichway.GridLocation.X) {
-            grid[((mazeCell.GridLocation.X - 1) * _size) + mazeCell.GridLocation.Y] = true;
-            Draw(mazeCell.GridLocation.X - 1, mazeCell.GridLocation.Y);
-        }
-        if (mazeCell.GridLocation.Y < whichway.GridLocation.Y) {
-            grid[(mazeCell.GridLocation.X * _size) + mazeCell.GridLocation.Y + 1] = true;
-            Draw(mazeCell.GridLocation.X, mazeCell.GridLocation.Y + 1);
-        }
-        if (mazeCell.GridLocation.Y <= whichway.GridLocation.Y) {
-            return;
-        }
-        grid[(mazeCell.GridLocation.X * _size) + mazeCell.GridLocation.Y - 1] = true;
-        Draw(mazeCell.GridLocation.X, mazeCell.GridLocation.Y - 1);
+var moveInDirection = function (directionList, mazeCell, grid) {
+    var direction = getRandomInt(0, directionList.length);
+    removeWall(mazeCell[mazeCell.length - 1], directionList[direction], grid);
+    mazeCell.push(directionList[direction]);
+    return mazeCell;
+};
+
+var buildMaze = function (mazeCell, grid) {
+    var directionList = getDirectionList(mazeCell[mazeCell.length - 1]);
+    if (mazeCell[mazeCell.length - 1].Visited && directionList.length > 0) {
+        return moveInDirection(directionList, mazeCell, grid);
     }
-
-    function BuildMaze(mazeCell, grid) {
-        var goingtopossibleList = GoingtopossibleList(mazeCell[mazeCell.length - 1]);
-        if (mazeCell[mazeCell.length - 1].Visited && goingtopossibleList.length > 0) {
-            var goingTo = getRandomInt(0, goingtopossibleList.length - 1);
-            Removewall(mazeCell[mazeCell.length - 1], goingtopossibleList[goingTo], grid);
-            mazeCell.push(goingtopossibleList[goingTo]);
-            return mazeCell;
-        }
-        RemoveCurrentPosition(mazeCell[mazeCell.length - 1], grid);
-        mazeCell[mazeCell.length - 1].Visited = true;
-        if (0 < goingtopossibleList.length) {
-            var whichway = getRandomInt(0, goingtopossibleList.length);
-            Removewall(mazeCell[mazeCell.length - 1], goingtopossibleList[whichway], grid);
-            mazeCell.push(goingtopossibleList[whichway]);
-            return mazeCell;
-        }
-        mazeCell.pop();
-        return mazeCell;
+    removeCurrentPosition(mazeCell[mazeCell.length - 1], grid);
+    mazeCell[mazeCell.length - 1].Visited = true;
+    if (0 < directionList.length) {
+        return moveInDirection(directionList, mazeCell, grid);
     }
+    mazeCell.pop();
+    return mazeCell;
+};
 
-    function RemoveCurrentPosition(mazeCell, grid) {
-        var x = mazeCell.GridLocation;
-        grid[x.X * _size + x.Y] = true;
-        Draw(x.X, x.Y);
-    }
-}
+var removeCurrentPosition = function (mazeCell, grid) {
+    var x = mazeCell.GridLocation;
+    grid[x.X * _size + x.Y] = true;
+    Draw(x.X, x.Y);
+};
 
-function InitializeCells(cells, size) {
+var initializeCells = function (cells, size) {
     for (var j = 0; j <= (size / 2) - 1; j++) {
         for (var i = 0; i <= (size / 2) - 1; i++) {
             cells.push(new Cell(new Point(i * 2 + 1, j * 2 + 1)));
         }
     }
-}
+};
 
-function GoingtopossibleList(mazeCell) {
-    var goingtopossibleList = [];
-    if (!mazeCell.Top.Visited) {
-        goingtopossibleList.push(mazeCell.Top);
+var removeWall = function (mazeCell, direction, grid) {
+    if (mazeCell.GridLocation.X < direction.GridLocation.X) {
+        grid[((mazeCell.GridLocation.X + 1) * _size) + mazeCell.GridLocation.Y] = true;
+        Draw(mazeCell.GridLocation.X + 1, mazeCell.GridLocation.Y);
     }
-    if (!mazeCell.Bottom.Visited) {
-        goingtopossibleList.push(mazeCell.Bottom);
+    if (mazeCell.GridLocation.X > direction.GridLocation.X) {
+        grid[((mazeCell.GridLocation.X - 1) * _size) + mazeCell.GridLocation.Y] = true;
+        Draw(mazeCell.GridLocation.X - 1, mazeCell.GridLocation.Y);
     }
-    if (!mazeCell.Right.Visited) {
-        goingtopossibleList.push(mazeCell.Right);
+    if (mazeCell.GridLocation.Y < direction.GridLocation.Y) {
+        grid[(mazeCell.GridLocation.X * _size) + mazeCell.GridLocation.Y + 1] = true;
+        Draw(mazeCell.GridLocation.X, mazeCell.GridLocation.Y + 1);
     }
-    if (!mazeCell.Left.Visited) {
-        goingtopossibleList.push(mazeCell.Left);
+    if (mazeCell.GridLocation.Y <= direction.GridLocation.Y) {
+        return;
     }
-    return goingtopossibleList;
-}
+    grid[(mazeCell.GridLocation.X * _size) + mazeCell.GridLocation.Y - 1] = true;
+    Draw(mazeCell.GridLocation.X, mazeCell.GridLocation.Y - 1);
+};
 
-function Draw(x, y) {
-    var context = document.getElementById('drawingarea').getContext("2d");
+var getDirectionList = function (mazeCell) {
+    var directionList = [];
+    for (var prop in mazeCell) {
+        if (!(prop === 'GridLocation' || prop === 'Visited')) {
+            if (!mazeCell[prop].Visited) {
+                directionList.push(mazeCell[prop]);
+            }
+        }
+    }
+    return directionList;
+};
+
+var Draw = function (x, y) {
     context.fillStyle = "#FFF";
     context.fillRect(x, y, 1, 1);
-}
+};
 
-function RenderWalls(grid, size) {
-    var context = document.getElementById('drawingarea').getContext("2d");
+var renderWalls = function (grid, size) {
     context.fillStyle = "#000";
     var _size = size + 1;
-    for (var count = 0; count < _size; count++) {
-        for (var count1 = 0; count1 < _size; count1++) {
-            if (!grid[count * _size + count1]) {
-                context.fillRect(count, count1, 1, 1);
+    for (var x = 0; x < _size; x++) {
+        for (var y = 0; y < _size; y++) {
+            if (!grid[x * _size + y]) {
+                context.fillRect(x, y, 1, 1);
             }
         }
     }
-}
+};
 
-function Point(x, y) {
+var Point = function (x, y) {
     this.X = x;
     this.Y = y;
-}
-
-function Cell(gridlocation) {
- return {
-    GridLocation: gridlocation,
-    Visited: false,
-    Top: null,
-    Bottom: null,
-    Left: null,
-    Right: null
 };
-}
 
-function getRandomInt(min, max) {
+var Cell = function (gridLocation) {
+    return {
+        GridLocation: gridLocation,
+        Visited: false,
+        Top: null,
+        Bottom: null,
+        Left: null,
+        Right: null
+    };
+};
+
+var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
-}
+};
 
-function SetupCellNeighbors(cells, size) {
+var setupCellNeighbors = function (cells, size) {
     var width = size / 2;
+    var lineWidth;
+    var targetCell;
     for (var j = 0; j <= width - 1; j++) {
         for (var i = 0; i <= width - 1; i++) {
-            var linewidth = i * width;
-            var workingOnThisOne = cells[linewidth + j];
-            if (i == 0) {
-                workingOnThisOne.Top = cells[linewidth + j];
+         lineWidth = i * width;
+         targetCell = cells[lineWidth + j];
+            if (i === 0) {
+                targetCell.Top = cells[lineWidth + j];
             }
-            if (j == 0) {
-                workingOnThisOne.Left = cells[linewidth + j];
+            if (j === 0) {
+                targetCell.Left = cells[lineWidth + j];
             }
-            if (i == width - 1) {
-                workingOnThisOne.Bottom = cells[linewidth + j];
+            if (i === width - 1) {
+                targetCell.Bottom = cells[lineWidth + j];
             }
-            if (j == width - 1) {
-                workingOnThisOne.Right = cells[linewidth + j];
+            if (j === width - 1) {
+                targetCell.Right = cells[lineWidth + j];
             }
-            if (workingOnThisOne.Top == null) {
-                workingOnThisOne.Top = cells[(i - 1) * width + j];
+            if (targetCell.Top == null) {
+                targetCell.Top = cells[(i - 1) * width + j];
             }
-            if (workingOnThisOne.Bottom == null) {
-                workingOnThisOne.Bottom = cells[(i + 1) * width + j];
+            if (targetCell.Bottom == null) {
+                targetCell.Bottom = cells[(i + 1) * width + j];
             }
-            if (workingOnThisOne.Left == null) {
-                workingOnThisOne.Left = cells[linewidth + (j - 1)];
+            if (targetCell.Left == null) {
+                targetCell.Left = cells[lineWidth + (j - 1)];
             }
-            if (workingOnThisOne.Right == null) {
-                workingOnThisOne.Right = cells[linewidth + (j + 1)];
+            if (targetCell.Right == null) {
+                targetCell.Right = cells[lineWidth + (j + 1)];
             }
         }
     }
-}
+};
